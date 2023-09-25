10153424d1fb51e56128191832d76fb225997c99
Mikkel Rasmus Schmidt <Mikkel.schmidt@hotmail.com>
2023-09-25 11:13:37 +0200

    gsdf

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -8,6 +8,41 @@ import openai
 
 openai.api_key = "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"
 
+## This code was written by OpenAI: https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb
+
+
+def vector_similarity(x: list[float], y: list[float]) -> float:
+    """
+    Returns the similarity between two vectors.
+    
+    Because OpenAI Embeddings are normalized to length 1, the cosine similarity is the same as the dot product.
+    """
+    return np.dot(np.array(x), np.array(y))
+
+def order_by_similarity(query: str, contexts: dict[(str, str), np.array]) -> list[(float, (str, str))]:
+    """
+    Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
+    to find the most relevant sections. 
+    
+    Return the list of document sections, sorted by relevance in descending order.
+    """
+    query_embedding = get_embedding(query)
+    
+    document_similarities = sorted([
+        (vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in contexts.items()
+    ], reverse=True)
+    
+    return document_similarities
+    
+MAX_SECTION_LEN = 2000
+SEPARATOR = "\n* "
+ENCODING = "gpt2"  # encoding for text-davinci-003
+
+encoding = tiktoken.get_encoding(ENCODING)
+separator_len = len(encoding.encode(SEPARATOR))
+
+f"Context separator contains {separator_len} tokens"
+
 
 with open('document_embeddings.pkl', 'rb') as fp:
     document_embeddings = pickle.load(fp)
