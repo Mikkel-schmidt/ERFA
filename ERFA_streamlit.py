ca304b497ec9336e7d835220d7b25523993ef499
Mikkel Rasmus Schmidt <Mikkel.schmidt@hotmail.com>
2023-09-25 11:19:19 +0200

    gwrs

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -16,6 +16,27 @@ df = pd.read_csv('df.csv')
 
 ## This code was written by OpenAI: https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb
 
+def get_embedding(text: str, model: str=EMBEDDING_MODEL) -> list[float]:
+    result = openai.Embedding.create(
+      model=model,
+      input=text
+    )
+    return result["data"][0]["embedding"]
+
+def compute_doc_embeddings(df: pd.DataFrame) -> dict[tuple[str, str], list[float]]:
+    """
+    Create an embedding for each row in the dataframe using the OpenAI Embeddings API.
+    
+    Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
+    """
+    return {
+        idx: get_embedding(r.content) for idx, r in df.iterrows()
+    }
+
+document_embeddings = compute_doc_embeddings(df)
+
+## This code was written by OpenAI: https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb
+
 
 def vector_similarity(x: list[float], y: list[float]) -> float:
     """
