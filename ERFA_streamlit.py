b051ef2a4f56e890279456dc72f9020c9e371fd9
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-11-08 17:07:04 +0100

    kilder instead of df

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -128,8 +128,8 @@ def construct_prompt(question: str, previous_questions, context_embeddings: dict
     #st.write("\n".join(chosen_sections))df_styled = df.head().style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])})
     kilder = df[['Kilde', 'url']].iloc[chosen_sections_indexes]
     for i in range(kilder.shape[0]):
-        url = df.iloc[i]['url'] 
-        st.write("[" + str(df.iloc[i]['Kilde']) + "](%s)" % url)
+        url = kilder.iloc[i]['url'] 
+        st.write("[" + str(kilder.iloc[i]['Kilde']) + "](%s)" % url)
     #st.write(df[['Kilde', 'url']].iloc[chosen_sections_indexes].style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])}))
         
     return chosen_sections, chosen_sections_len
