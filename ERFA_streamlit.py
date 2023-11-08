4cc6f650decf9310f18b9502600abf49d15aed34
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-11-08 16:57:59 +0100

    csad

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -221,8 +221,8 @@ if prompt := st.chat_input('Indtast spørgsmål til ERFA-bladene, sikkerhedsstyr
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response, sections_tokens = get_response(INSTRUCTIONS, st.session_state.messages, prompt, df, document_embeddings)
-    st.write(response)
+    #st.write(response)
     msg = response#.choices[0].message
     st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    st.chat_message("assistant").write(msg)
 
