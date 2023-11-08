31c746716305e4f78f76cccdd7c273e464f71436
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-11-08 16:48:52 +0100

    vsfd

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -127,6 +127,7 @@ def construct_prompt(question: str, previous_questions, context_embeddings: dict
     st.write(f"Vigtigste {len(chosen_sections)} kilder:")
     #st.write("\n".join(chosen_sections))df_styled = df.head().style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])})
     kilder = df[['Kilde', 'url']].iloc[chosen_sections_indexes]
+    st.write(kilder)
     for i in kilder.shape[0]:
         url = df.iloc[i]['url']
         print(url)
