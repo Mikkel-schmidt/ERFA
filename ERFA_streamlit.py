491b16727f1a26b943db5c08d378761c19564317
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-11-08 16:54:39 +0100

    hbgfrt

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -127,8 +127,6 @@ def construct_prompt(question: str, previous_questions, context_embeddings: dict
     st.write(f"Vigtigste {len(chosen_sections)} kilder:")
     #st.write("\n".join(chosen_sections))df_styled = df.head().style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])})
     kilder = df[['Kilde', 'url']].iloc[chosen_sections_indexes]
-    st.write(kilder)
-    st.write(kilder.shape[0])
     for i in range(kilder.shape[0]):
         url = df.iloc[i]['url']
         st.write("[" + str(df.iloc[i]['Kilde']) + "](%s)" % url)
