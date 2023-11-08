cc35aecc556aafd64b30c0086b81dbf07253b78c
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-11-08 16:55:31 +0100

    deqw

--- ERFA_streamlit.py
+++ ERFA_streamlit.py
@@ -128,7 +128,7 @@ def construct_prompt(question: str, previous_questions, context_embeddings: dict
     #st.write("\n".join(chosen_sections))df_styled = df.head().style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])})
     kilder = df[['Kilde', 'url']].iloc[chosen_sections_indexes]
     for i in range(kilder.shape[0]):
-        url = df.iloc[i]['url']
+        url = df.iloc[i]['url'] 
         st.write("[" + str(df.iloc[i]['Kilde']) + "](%s)" % url)
     #st.write(df[['Kilde', 'url']].iloc[chosen_sections_indexes].style.format({'url': lambda x: make_clickable(x, df.loc[df['url'] == x]['Kilde'].values[0])}))
         
